b80f39e5b604b825c6577d11e44ba9f0f61835b9
novicecivon <novicecivon@protonmail.com>
2023-12-30 14:14:49 +0800

    Task 2 Deployment completed: - Added notes - Added dockerfile

--- Task2/app.py
+++ Task2/app.py
@@ -4,27 +4,20 @@ import gradio as gr
 import dotenv
 
 dotenv.load_dotenv()
-
 aai.settings.api_key = os.getenv("AAI_API_KEY")
 transcriber = aai.Transcriber()
 
-# transcript = transcriber.transcribe(
-#     "https://storage.googleapis.com/aai-web-samples/news.mp4"
-# )
-# # transcript = transcriber.transcribe("./my-local-audio-file.wav")
-
-# print(transcript.text)
-
 
 def transcribe_audio(audio_file):
+    # Adapt this line for Lambda's temp file system if needed
     assert os.path.exists(audio_file)
     transcript = transcriber.transcribe(audio_file).text.replace(".", "")
     output_transcript = []
     for word in transcript.split():
         if word.endswith(("a", "e", "i", "o", "u")):
-            output_transcript.append(f"{word}-v")  # Add "-v" suffix
+            output_transcript.append(f"{word}-v")
         else:
-            output_transcript.append(f"{word}-c")  # Add "-c" suffix
+            output_transcript.append(f"{word}-c")
     return " ".join(output_transcript)
 
 
@@ -33,5 +26,7 @@ iface = gr.Interface(
     inputs=gr.Audio(sources=["microphone", "upload"], type="filepath"),
     outputs="text",
     title="Speech-to-Text Transcription",
+    flagging_dir="/tmp/flagged",
 )
-iface.launch(share=True)
+
+iface.launch(server_port=8080)
